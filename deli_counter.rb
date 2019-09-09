ecfe77034b090b1e9fb6cfc0edf1a6ba7c63ab6f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-09 21:23:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(array)
     puts "The line is currently empty."
   else
     i = 0
-    print "The line is currently: "
+    line = "The line is currently: "
     while (i < array.size)
-      print "#{i+1}. #{array[i]} "
+      line << "#{i+1}. #{array[i]} "
       i += 1
     end
+    puts line
   end
 end
 

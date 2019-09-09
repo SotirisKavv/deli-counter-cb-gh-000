b6ab9327a5086d181768492f589be6f3cd7f488f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-09 21:12:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  if (line == nil)
+    puts "The line is currently empty."
+  else
+    i = 0
+    print "The line is currently: "
+    while (i < array.size)
+      print "#{i+1}. #{array[i]}"
+      i += 1
+    end
+  end
+end
